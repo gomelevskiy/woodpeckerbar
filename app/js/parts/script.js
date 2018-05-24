db95c8c1e753c68bc1b2dea48f1b6164456e011f
админ <admin@iMac.local>
2018-05-24 16:07:20 +0300

    all sections and mobile version

--- app/js/parts/script.js
+++ app/js/parts/script.js
@@ -7,3 +7,42 @@ $(function () {
         }
     });
 });
+
+$(document).ready(function(){
+    var section_items=$('.section'),
+        navigation_items=$('#navigation a');
+
+    updateNavigation();
+    $(window).on('scroll', function(){
+        updateNavigation();
+    });
+
+    navigation_items.on('click',function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    $('.scroll_down').on('click',function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    function updateNavigation() {
+        section_items.each(function(){
+            $this = $(this);
+            var activeSection = $('#navigation a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+            if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+                navigation_items.eq(activeSection).addClass('active');
+            }else {
+                navigation_items.eq(activeSection).removeClass('active');
+            }
+        });
+    }
+
+    function smoothScroll(target){
+        $('body,html').animate(
+            {'scrollTop':target.offset().top}
+            ,600);
+    }
+
+});
